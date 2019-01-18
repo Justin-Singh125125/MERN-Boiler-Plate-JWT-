f0e0268c46e9613f7c23c102ca3493be1fedcf4f
Justin Singh <justin.singh125125@gmail.com>
2019-01-18 09:01:35 -0800

    added boiler plate items

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Books from "./pages/Books";
+// import Detail from "./pages/Detail";
+// import NoMatch from "./pages/NoMatch";
+// import Nav from "./components/Nav";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div>hello world</div>
+  );
+}
+
+export default App;
